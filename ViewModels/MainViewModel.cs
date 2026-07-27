44c848a478969754df64e0159a83deaf051586eb
Joeldev0208 <Joelgo0208@gmail.com>
2026-07-27 17:27:19 -0400

    archivos de referencia

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -5,5 +5,5 @@ namespace learn_Assist.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    public partial string Greeting { get; set; } = "hello ";
 }
